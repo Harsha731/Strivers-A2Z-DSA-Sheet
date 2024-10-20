49e7f67adcfc3427193465a26096a3732c7108b7
Spiderman731 <harshapraneeth51@gmail.com>
2024-10-20 16:25:33 +0530

    Update 04. Maximum K sum combinations.cpp

--- 09. Heaps/3. Hard Problems/04. Maximum K sum combinations.cpp
+++ 09. Heaps/3. Hard Problems/04. Maximum K sum combinations.cpp
@@ -36,24 +36,23 @@ CODE:-
 */
 
 vector<int> maxCombinations(int N, int K, vector<int> &A, vector<int> &B) {
-    priority_queue<pair<int,pair<int,int>>> pq;
-    sort(A.begin(),A.end());
-    sort(B.begin(),B.end());
-    for(int i=0;i<N;i++){
-        pq.push({A[i]+B[N-1],{i,N-1}});
-    }
+    priority_queue<pair<int, pair<int, int>>> pq;
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+
+    for (int i = 0; i < N; ++i)
+        pq.push({A[i] + B[N - 1], {i, N - 1}});
+
     vector<int> ans;
-    while(!pq.empty() && K--)
-    {
-        auto it=pq.top();
+    while (K-- && !pq.empty()) {
+        auto [sum, pos] = pq.top();
         pq.pop();
-        int data=it.first;
-        int x=it.second.first;
-        int y=it.second.second;
-        ans.push_back(data);
-        if(y!=0){
-            pq.push({A[x]+B[y-1],{x,y-1}});
-        }
+        ans.push_back(sum);
+
+        int x = pos.first, y = pos.second;
+        if (y > 0)
+            pq.push({A[x] + B[y - 1], {x, y - 1}});
     }
+
     return ans;
 }
