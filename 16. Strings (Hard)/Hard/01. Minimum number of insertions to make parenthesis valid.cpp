77e20406063a795973cbcf77715eaf5660a188fc
Spiderman731 <harshapraneeth51@gmail.com>
2024-10-26 20:26:03 +0530

    Update 01. Minimum number of insertions to make parenthesis valid.cpp
    Signed-off-by: Spiderman731 <harshapraneeth51@gmail.com>

--- 16. Strings (Hard)/Hard/01. Minimum number of insertions to make parenthesis valid.cpp
+++ 16. Strings (Hard)/Hard/01. Minimum number of insertions to make parenthesis valid.cpp
@@ -43,3 +43,26 @@ int minAddToMakeValid(string s) {
     }
     return open + close; // total moves required
 }
+
+
+// Naive
+// TC : O(N) and SC : O(N)
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> S;
+        int c = 0;
+        for (char i : s) {
+            if (i == '(')
+                S.push(i);
+            else {
+                if (S.empty())
+                    c++;
+                else
+                    S.pop();
+            }
+        }
+        return c + S.size();
+    }
+};
