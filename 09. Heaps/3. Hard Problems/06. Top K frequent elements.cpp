f31b088a09ef1d4c4de486542cea4851dbbea58c
Spiderman731 <harshapraneeth51@gmail.com>
2024-10-27 16:15:43 +0530

    Update 06. Top K frequent elements.cpp
    Signed-off-by: Spiderman731 <harshapraneeth51@gmail.com>

--- 09. Heaps/3. Hard Problems/06. Top K frequent elements.cpp
+++ 09. Heaps/3. Hard Problems/06. Top K frequent elements.cpp
@@ -1,24 +1,4 @@
-/*
-Question:
-Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
-*/
 
-/*
-Approach:
-1. Create a frequency map to count the occurrences of each element in the array.
-2. Use a min heap to store the k most frequent elements based on their frequencies.
-3. Iterate through the frequency map and push elements into the min heap.
-4. If the size of the min heap exceeds k, remove the element with the lowest frequency.
-5. Finally, extract the elements from the min heap and return them in reverse order to get the k most frequent elements.
-*/
-
-/*
-Complexity Analysis:
-- The time complexity is O(N log K), where N is the size of the input array nums. Building the frequency map takes O(N) time, and inserting and removing elements from the min heap take O(log K) time, which is done for each element in nums.
-- The space complexity is O(N) to store the frequency map and O(K) for the min heap, resulting in a total of O(N + K) space.
-
-CODE:-
-*/
 
 vector<int> topKFrequent(vector<int>& nums, int k) {
     unordered_map<int, int> mp;
