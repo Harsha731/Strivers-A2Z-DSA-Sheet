5d28bf01e51dd03427de47ef59441d42d129c9ca
Spiderman731 <harshapraneeth51@gmail.com>
2024-10-31 15:29:53 +0530

    Update 03. Prefix to infix.cpp
    Signed-off-by: Spiderman731 <harshapraneeth51@gmail.com>

--- 07.Stack and Queues/2. Infix, Postfix, and Prefix/03. Prefix to infix.cpp
+++ 07.Stack and Queues/2. Infix, Postfix, and Prefix/03. Prefix to infix.cpp
@@ -14,33 +14,25 @@ APPROACH:
 - We can use a stack to convert the prefix expression to infix.
 - We iterate through each character of the input string in reverse order.
 - If the character is an operand (letter or digit), we push it onto the stack.
-- If the character is an operator, we pop two operands from the stack, concatenate them with the operator and enclosing parentheses, and push the result back onto the stack.
+- If the character is an operator (+, -, *, /, ^), we pop two operands from the stack, concatenate them with the operator and enclosing parentheses, and push the result back onto the stack.
 - After iterating through all characters, the top of the stack will contain the final infix expression.
 
 CODE:
 */
 
-string preToInfix(string pre_exp) {
+string PREFIX_INFIX(const string& s) {
     stack<string> st;
-    
-    for (int i = pre_exp.size() - 1; i >= 0; i--) {
-        char ch = pre_exp[i];
-        
-        if (('A' <= ch && ch <= 'Z') || ('a' <= ch && ch <= 'z') || ('0' <= ch && ch <= '9')) {
-            string temp = "";
-            temp += ch;
-            st.push(temp);
+
+    for (int i = s.size() - 1; i >= 0; i--) {
+        if (isalnum(s[i])) {  // Check if the character is alphanumeric
+            st.push(string(1, s[i]));
         } else {
-            string a = st.top();
-            st.pop();
-            string b = st.top();
-            st.pop();
-            
-            string temp = "(" + a + ch + b + ")";
-            st.push(temp);
+            string op1 = st.top(); st.pop();
+            string op2 = st.top(); st.pop();
+            st.push("(" + op1 + s[i] + op2 + ")");
         }
     }
-    
+
     return st.top();
 }
 
