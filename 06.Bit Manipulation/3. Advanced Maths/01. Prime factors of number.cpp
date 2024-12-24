f0b0e35a1e9c6afffb9717146e83f05d24cde47a
Harsha731 <harshapraneeth51@gmail.com>
2024-12-24 11:41:48 +0530

    Update 01. Prime factors of number.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 06.Bit Manipulation/3. Advanced Maths/01. Prime factors of number.cpp
+++ 06.Bit Manipulation/3. Advanced Maths/01. Prime factors of number.cpp
@@ -18,83 +18,76 @@ To find the unique prime factors of a number N, we can iterate from 2 to sqrt(N)
 4. Return `ans`.
 
 CODE:*/
-vector<int> AllPrimeFactors(int n) {
-    vector<int> ans;
+// Function to check if a number is prime
+bool prime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+// method 1
+
+vector<int> findPrimeFactorsMethod1(int n) {
+    vector<int> list;
+
+    // TC is O(N * root(N))
+    // SC depends on input, as we are not using any space for space but only for output list
+    for (int i = 2; i <= n; i++) {
+        if (n % i == 0) {
+            if (prime(i)) list.push_back(i);
+        }
+    }
+
+    return list;
+}
+
+// method 2
+
+vector<int> findPrimeFactorsMethod2(int n) {
+    vector<int> list;
+
+    // TC is O(root(N) * root(N))
     for (int i = 2; i * i <= n; i++) {
         if (n % i == 0) {
-            ans.push_back(i);
-            while (n % i == 0)
-                n /= i;
+            if (prime(i)) list.push_back(i);
+            if (n / i != i) { // not a perfect square like 6*6 = 36
+                if (prime(n / i)) list.push_back(n / i);
+            }
         }
     }
-    if (n > 1)
-        ans.push_back(n);
-    return ans;
+
+    return list;
 }
 
-list = [];
+// method 3
 
-method 1)
+vector<int> findPrimeFactorsMethod3(int n) {
+    vector<int> list;
 
-// TC is O(N*root(N))
-// SC depends on input, as we are not using any space for space but only for output list
-for (int i=2;i<=n;i++){
-    if(n%i == 0){
-        if(prime(i)) list.add(i);
-    }
-
-    
-}
-
-method 2)
-
-// TC is O(root(N)*root(N))
-for (int i=2; i*i<=n; i++){
-    if(n%i == 0){
-        if(prime(i)) list.add(i);
-    	if(n/i != i){ 			// not a perfect square like 6*6 = 36
-        	if(prime(n/i)) list.add(n/i);
-	}
-    }    
-}
-
-// leave this
-
-// TC is O(N) in worst case, but can reduce very much in average cases
-// same like normal division
-for (int i=2;i<=n;i++){
-    if(n%i == 0){
-        if(prime(i)) list.add(i);
-        while(n%i==0) {
-            n = n/i;
+    // Worst-case TC is O(root(N) * logN)
+    // logN is written when we do divisions
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            if (prime(i)) list.push_back(i);
+            while (n % i == 0) {
+                n = n / i;
+            }
+            // Here, unlike the 2nd method, we need not check n/i != i
+            // as anyways we are doing it by n = n/i
         }
     }
+    if (n != 1) list.push_back(n);
+    // Last line is important as there are 2 cases.
+    // In most cases, it is like we had checked from i = 2 step by step and divided the number till we can.
+    // Ex: 42 = 2, root(42). 3, root(21). 4, root(7). We have to check from 2 to root(7) for number 7.
+    // But already 2, 3 are over. So, the only option left was 7.
+
+    // But only in the case of powers of 2, n is 1. Here we can't add 1 as it is not prime.
+
+    return list;
 }
-
-method 3)
-
-// worst case TC is O(root(N)*logN)
-//logN is written when we do divisions
-for (int i=2; i*i<=n; i++){
-    if(n%i == 0){
-        if(prime(i)) list.add(i);
-        while(n%i==0) {
-            n = n/i;
-        }
-// Here, unline the 2nd methos, we need not do check n/i != i 
-// as anyways we are doing it by n = n/i
-    }
-}
-if(n!=1) list.add(n);
-// Last line is important as there are 2 cases. 
-// In most of the cases, it is like we had checked from i=2 step by step and divided the number till we can
-// ex is 42 = 2,root(42). 3,root(21). 4,root(7). We have to check from 2 to root(7) for number 7. 
-// But already 2,3 over. So, only option left was 7.
-
-// But only in the case of power of 2, the n is 1, here we can't add 1 as it is not prime.
-
-
-
 
 // Time Complexity: O(sqrt(N))
 // Space Complexity: O(1) (excluding the space required for the output vector)
