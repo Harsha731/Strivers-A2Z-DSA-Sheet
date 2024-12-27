6f0c751e173c797747f05b5a89e8af2874a89cf8
Harsha731 <harshapraneeth51@gmail.com>
2024-12-27 19:34:31 +0530

    Update 03.Copy_LL_with_random_pointers.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 04.Linked List/5.Hard Promblems of LL/03.Copy_LL_with_random_pointers.cpp
+++ 04.Linked List/5.Hard Promblems of LL/03.Copy_LL_with_random_pointers.cpp
@@ -24,6 +24,19 @@ Example 2:
 Input: head = [[1,1],[2,1]]
 Output: [[1,1],[2,1]]
 
+Brute Force APPROACH :
+
+Step 1:Initialise variables ‘temp’ as a pointer to the head of the original linked list to traverse it. Create an empty unordered_map, to map original nodes to their corresponding copied nodes.
+
+Step 2: Iterate through the original linked list and for each node in the linked list create a new node with the same data value as the original data. Map the original node to its copied node in the map.
+
+Step 3: Iterate through the original list again but this time connect the pointers of the copied nodes in the same arrangement as the original node.
+Get the copied node corresponding to the original node using the map.
+Set the next pointer of the copied node to the copied node mapped to the original node’s next node.
+Set the random pointer of the copied node to the original node’s next node copied from the map.
+
+Step 4: Return the head of the deep copied list which is obtained by retrieving the copied nodes mapped to the original head from the map.
+
 APPROACH:
 1. Insert Copy Nodes in Between:
 Traverse the original list.
