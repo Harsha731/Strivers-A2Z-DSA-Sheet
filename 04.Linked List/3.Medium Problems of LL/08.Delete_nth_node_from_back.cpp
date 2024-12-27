333182086bde269ee4e64d59503f3a65730fcce9
Harsha731 <harshapraneeth51@gmail.com>
2024-12-27 15:36:30 +0530

    Update 08.Delete_nth_node_from_back.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 04.Linked List/3.Medium Problems of LL/08.Delete_nth_node_from_back.cpp
+++ 04.Linked List/3.Medium Problems of LL/08.Delete_nth_node_from_back.cpp
@@ -15,6 +15,16 @@ Input: head = [1,2], n = 1
 Output: [1]
 */
 
+/* 
+1. Initialize a temp pointer to traverse the linked list.
+2. Use a cnt variable to count the total nodes in the list by iterating until temp reaches NULL
+3. Compute the position of the node to delete as L - N + 1.
+4. Traverse the list using a new temp pointer until it points to the (L - N)-th node.
+5. Adjust the link of the (L - N)-th node to skip the (L - N + 1)-th node.
+6. Free the memory of the deleted node (if applicable).
+Note: For languages with automatic garbage collection (e.g., Java, Python, JavaScript), explicit memory freeing isn't needed.
+*/
+
 /*
 APPROACH:
 To remove the nth node from the end of the linked list, we can follow these steps:
