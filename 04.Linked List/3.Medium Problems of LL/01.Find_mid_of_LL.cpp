b748fc9907ac009f0e399dd5e38aa85452e1e90f
Harsha731 <harshapraneeth51@gmail.com>
2024-12-27 12:01:43 +0530

    Update 01.Find_mid_of_LL.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 04.Linked List/3.Medium Problems of LL/01.Find_mid_of_LL.cpp
+++ 04.Linked List/3.Medium Problems of LL/01.Find_mid_of_LL.cpp
@@ -15,6 +15,15 @@ Explanation: Since the list has two middle nodes with values 3 and 4, we return 
 */
 
 /*
+Step 1: Initialise pointer temp to the head of the linked list and a variable to count to hold the number of nodes in the linked list. Using temp traverse the linked list,
+increasing the value of count by one at each node till temp becomes null. The final value of count will represent the length of the linked list.
+Step 2: Calculate mid as count/2 + 1 where count is the length of the linked list. Mid represents the position of the middle node.
+Step 3: Reset temp pointer back to the head and traverse the list by iteratively moving temp mid number of times. The node pointed to by current after this traversal is the middle node of the linked list.
+
+*/
+
+
+/*
 APPROACH:
 -> To find the middle node of a linked list, we can use the two-pointer technique.
 -> Initialize two pointers, slow and fast, to the head of the linked list.
