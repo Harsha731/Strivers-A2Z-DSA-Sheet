15889bdbf0b1bcc7cfad0a414c2cf27d80f4e9c2
Harsha731 <harshapraneeth51@gmail.com>
2025-01-01 23:14:09 +0530

    Update 01. Height of binary tree.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 11. Binary Trees/2. Medium Problems/01. Height of binary tree.cpp
+++ 11. Binary Trees/2. Medium Problems/01. Height of binary tree.cpp
@@ -3,9 +3,11 @@ Question:
 Given the root of a binary tree, return its maximum depth.
 A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
-Approach:
+Approach 1:
 - We can calculate the maximum depth of a binary tree recursively by traversing its left and right subtrees.
 - The maximum depth of a tree is equal to the maximum of the maximum depths of its left and right subtrees, plus 1 for the root node.
+
+Approach 2: Level Order Traversal - iterative approach
 
 Complexity Analysis:
 - Since we visit each node once and perform constant time operations for each node, the time complexity of this approach is O(N), where N is the number of nodes in the binary tree.
@@ -14,9 +16,11 @@ Complexity Analysis:
 Code:
 */
 
-int maxDepth(TreeNode* root) {
-    if (!root) {
+int maxDepth(Node* root){
+    if(root == NULL){
         return 0;
     }
-    return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    int lh = maxDepth(root->left);
+    int rh = maxDepth(root->right);
+    return 1 + max(lh, rh);
 }
