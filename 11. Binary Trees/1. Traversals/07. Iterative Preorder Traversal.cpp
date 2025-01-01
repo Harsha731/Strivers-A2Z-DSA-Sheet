7c2d7c6141f248be7755eec1c1f3a2b62e721732
Harsha731 <harshapraneeth51@gmail.com>
2025-01-01 14:05:00 +0530

    Update 07. Iterative Preorder Traversal.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 11. Binary Trees/1. Traversals/07. Iterative Preorder Traversal.cpp
+++ 11. Binary Trees/1. Traversals/07. Iterative Preorder Traversal.cpp
@@ -1,37 +1,46 @@
 /*
 Question:
 Given the root of a binary tree, return the preorder traversal of its nodes' values.
-
-Approach:
-- We can perform a preorder traversal iteratively using a stack.
-- We start by pushing the root node into the stack.
-- Then, while the stack is not empty, we pop a node from the stack, add its value to the result vector, and push its right child (if it exists) followed by its left child (if it exists) into the stack.
-- By pushing the right child before the left child, we ensure that the left child is processed first during the traversal.
-
-Complexity Analysis:
-- Since we visit each node once and perform constant time operations for each node, the time complexity of this approach is O(N), where N is the number of nodes in the binary tree.
-- The space complexity is O(N) as we store the node values in the result vector.
-
-Code:
 */
 
-vector<int> preorderTraversal(TreeNode* root) {
-    if (!root) {
-        return {};
-    }
-    stack<TreeNode*> st;
-    st.push(root);
-    vector<int> ans;
-    while (!st.empty()) {
-        TreeNode* curr = st.top();
-        st.pop();
-        ans.push_back(curr->val);
-        if (curr->right) {
-            st.push(curr->right);
+/* 
+Push Root to Stack: Start traversal by pushing the root onto the stack.
+Iterative Traversal:
+    • Process Node: Pop the top node from the stack and add its value to the result.
+    • Right Child: If it exists, push the right child onto the stack.
+    • Left Child: If it exists, push the left child onto the stack (it gets processed before the right child).
+Return Result: Once the stack is empty, return the preorder traversal result.
+
+TC : O(N) and SC : O(N)
+*/ 
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        
+        if(root == nullptr) {
+            return preorder;
         }
-        if (curr->left) {
-            st.push(curr->left);
+        
+        stack<TreeNode*> st;
+        st.push(root);
+        
+        while(!st.empty()) {
+            root = st.top();
+            st.pop();
+            
+            preorder.push_back(root->val);
+            
+            if(root->right != nullptr) {
+                st.push(root->right);
+            }
+            
+            if(root->left != nullptr) {
+                st.push(root->left);
+            }
         }
+        
+        return preorder;
     }
-    return ans;
-}
+};
