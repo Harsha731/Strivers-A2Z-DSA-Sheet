e6fb08268b280c1cd45b263fe5f73c8f5c76bb22
Harsha731 <harshapraneeth51@gmail.com>
2025-01-02 23:08:33 +0530

    Update 11. Recover BST.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 12. Binary Search Trees/2. Practice Problems/11. Recover BST.cpp
+++ 12. Binary Search Trees/2. Practice Problems/11. Recover BST.cpp
@@ -2,11 +2,6 @@
 Question:
 You are given the root of a binary search tree (BST), where the values of exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
 
-Explanation:
-- We perform an in-order traversal of the BST to find the two nodes that are swapped.
-- During the in-order traversal, we keep track of the previous node and compare it with the current node. If the current node is less than the previous node, then we have found the two swapped nodes.
-- We swap the values of the two nodes to recover the BST.
-
 Time Complexity:
 - The in-order traversal takes O(n) time, where n is the number of nodes in the BST.
 
@@ -15,26 +10,79 @@ Space Complexity:
 
 */
 
-TreeNode* prev = NULL;
-TreeNode* first = NULL;
-TreeNode* last = NULL;
+/*
+Using 4 pointers - prev, first, middle, last
 
-void traversal(TreeNode* root) {
-    if (!root) return;
-    traversal(root->left);
-    if (prev && root->val < prev->val) {
-        if (!first)
-            first = prev;
-        last = root;
+1. Swapped nodes are not adjacent
+3 25 7 8 10 15 20 5
+Here, there are 2 violations for l > r
+In the first violation, store the left (with the variable name as first) (right of first violation is called as middle)
+In the second violation, store the right (with the variable name as last)
+Now, swap the first and last
+
+2. Swapped nodes are adjacent
+If we have only 1 violation, then simply swap the 2 numbers (first and middle)
+*/
+
+class Solution {
+private:
+    TreeNode *first, *prev, *middle, *last;
+
+    void inorder(TreeNode* root) {
+        if (!root) return;
+        inorder(root->left);
+        if (prev && root->val < prev->val) {
+            if (!first) {
+                first = prev;
+                middle = root;      // observe
+            } else {
+                last = root;        // observe
+            }
+        }
+        prev = root;
+        inorder(root->right);
     }
-    prev = root;
-    traversal(root->right);
-}
 
-void recoverTree(TreeNode* root) {
-    if (!root) return;
-    traversal(root);
-    int temp = first->val;
-    first->val = last->val;
-    last->val = temp;
-}
+public:
+    void recoverTree(TreeNode* root) {
+        first = middle = last = prev = nullptr;
+        inorder(root);
+        if (first && last) {
+            swap(first->val, last->val);
+        } else if (first && middle) {
+            swap(first->val, middle->val);
+        }
+    }
+};
+__________________________________________
+
+// We can remove the middle pointer by making last = root instead of middle = root
+
+class Solution {
+private:
+    TreeNode *first, *prev, *last;
+
+    void inorder(TreeNode* root) {
+        if (!root) return;
+        inorder(root->left);
+        if (prev && root->val < prev->val) {
+            if (!first) {
+                first = prev;   // unique to only for first time
+            }
+            last = root;
+        }
+        prev = root;
+        inorder(root->right);
+    }
+
+public:
+    void recoverTree(TreeNode* root) {
+        first = last = prev = nullptr;
+        inorder(root);
+        if (first && last) {
+            swap(first->val, last->val);
+        }
+    }
+};
+
+
