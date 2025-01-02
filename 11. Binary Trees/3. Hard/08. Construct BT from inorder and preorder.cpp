92cb1880ed04973cf9d77492ec6ddd86088e1df2
Harsha731 <harshapraneeth51@gmail.com>
2025-01-02 14:00:24 +0530

    Update 08. Construct BT from inorder and preorder.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 11. Binary Trees/3. Hard/08. Construct BT from inorder and preorder.cpp
+++ 11. Binary Trees/3. Hard/08. Construct BT from inorder and preorder.cpp
@@ -4,15 +4,10 @@ Given two integer arrays preorder and inorder where preorder is the preorder tra
 
 APPROACH:
 
-The preorder traversal follows the root-left-right order, while the inorder traversal follows the left-root-right order. Based on these two traversals, we can construct the binary tree recursively.
-
-1. The first element in the preorder traversal array is the root of the binary tree.
-2. We can find the position of the root in the inorder traversal array. All the elements to the left of this position will be in the left subtree, and all the elements to the right will be in the right subtree.
-3. Recursively, we can build the left and right subtrees using the corresponding portions of the preorder and inorder traversal arrays.
-4. The recursive function takes the index of the current element in the preorder traversal array, the starting and ending indices of the current portion in the inorder traversal array, and the preorder and inorder traversal arrays as inputs.
-5. In each recursive call, we create a new node with the value of the current element in the preorder traversal array and determine its left and right subtrees by calling the recursive function on the corresponding portions of the inorder and preorder traversal arrays.
-6. The base case is when the starting index is greater than the ending index, indicating an empty portion of the tree. In this case, we return NULL.
-7. Finally, we return the root of the constructed binary tree.
+Base Case: If the current range is invalid (preStart > preEnd or inStart > inEnd), return NULL.
+Root Identification: The root is the first element in the preorder array. Find its index in the inorder array using the map.
+Divide Tree: Use the root’s index to divide the tree into left and right subtrees.
+Recursion: Recursively build the left and right subtrees by adjusting the range of the preorder and inorder arrays.
 
 COMPLEXITY ANALYSIS:
 
@@ -23,23 +18,18 @@ Let n be the number of nodes in the binary tree.
 CODE:
 */
 
-TreeNode* solve(int i, int ins, int ine, vector<int>& preorder, vector<int>& inorder) {
-    if (i >= preorder.size() || ins > ine) return NULL;
-
-    int loc = -1;
-    for (int j = ins; j <= ine; j++) {
-        if (inorder[j] == preorder[i])
-            loc = j;
+TreeNode* buildTree(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd, map<int, int>& inMap){
+    if(preStart > preEnd || inStart > inEnd){
+        return NULL;
     }
-    TreeNode* root = new TreeNode(preorder[i]);
-    root->left = solve(i + 1, ins, loc - 1, preorder, inorder);
-    // Note: the index of the right child in preorder is the number of nodes in the left subtree + 1
-    root->right = solve(i + (loc - ins + 1), loc + 1, ine, preorder, inorder);
-
+    
+    TreeNode* root = new TreeNode(preorder[preStart]);
+    int inRoot = inMap[root->val];
+    int numsLeft = inRoot - inStart;
+    
+    root->left = buildTree(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1, inMap);
+    root->right = buildTree(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd, inMap);
+    
     return root;
 }
 
-TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-    int n = inorder.size();
-    return solve(0, 0, n - 1, preorder, inorder);
-}
