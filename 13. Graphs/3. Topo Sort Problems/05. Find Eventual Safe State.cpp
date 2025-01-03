09b57722fcec788b47083104f8754385817960f7
Harsha731 <harshapraneeth51@gmail.com>
2025-01-03 16:58:14 +0530

    Update 05. Find Eventual Safe State.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 13. Graphs/3. Topo Sort Problems/05. Find Eventual Safe State.cpp
+++ 13. Graphs/3. Topo Sort Problems/05. Find Eventual Safe State.cpp
@@ -1,14 +1,18 @@
 /*
 QUESTION:
-There is a directed graph of n nodes with each node labeled from 0 to n - 1. The graph is represented by a 0-indexed 2D integer array graph where graph[i] is an integer array of nodes adjacent to node i, meaning there is an edge from node i to each node in graph[i].
+There is a directed graph of n nodes with each node labeled from 0 to n - 1. The graph is represented by a 0-indexed 
+2D integer array graph where graph[i] is an integer array of nodes adjacent to node i, meaning there is an edge from 
+node i to each node in graph[i].
 
-A node is a terminal node if there are no outgoing edges. A node is a safe node if every possible path starting from that node leads to a terminal node (or another safe node).
+A node is a terminal node if there are no outgoing edges. A node is a safe node if every possible path starting 
+from that node leads to a terminal node (or another safe node).
 
 APPROACH:
 - We are given a directed graph, and we need to find all the safe nodes.
 - A node is safe if every possible path starting from that node leads to a terminal node (or another safe node).
 - To solve this, we can use a reverse graph approach along with a topological sorting method.
-- We create a reverse adjacency list, where revadj[i] contains all the nodes from which there is a directed edge to node i (i.e., nodes that have an edge towards node i).
+- We create a reverse adjacency list, where revadj[i] contains all the nodes from which there is a directed edge 
+to node i (i.e., nodes that have an edge towards node i).
 - We also keep track of the outdegree of each node, which represents the number of outgoing edges from that node.
 - We then start with nodes that have an outdegree of 0 (i.e., terminal nodes) and perform a BFS-like traversal.
 - During the traversal, we keep reducing the outdegree of the nodes from which there is a directed edge to the current node.
@@ -16,8 +20,10 @@ APPROACH:
 - All the nodes that become terminal nodes during the traversal are safe nodes.
 
 COMPLEXITY ANALYSIS:
-- Time Complexity: O(N + E), where N is the number of nodes, and E is the number of edges in the graph. We perform a BFS-like traversal of all nodes and edges.
-- Space Complexity: O(N + E), where N is the number of nodes, and E is the number of edges in the graph. We use additional space to store the reverse adjacency list and outdegree of each node.
+- Time Complexity: O(N + E), where N is the number of nodes, and E is the number of edges in the graph. 
+We perform a BFS-like traversal of all nodes and edges.
+- Space Complexity: O(N + E), where N is the number of nodes, and E is the number of edges in the graph. 
+We use additional space to store the reverse adjacency list and outdegree of each node.
 */
 
 vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
