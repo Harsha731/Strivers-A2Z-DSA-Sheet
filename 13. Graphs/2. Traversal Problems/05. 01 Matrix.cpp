58dccfc21e3d6141d7b56d9a3e477b447e06cdd2
Harsha731 <harshapraneeth51@gmail.com>
2025-01-03 13:49:36 +0530

    Update 05. 01 Matrix.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 13. Graphs/2. Traversal Problems/05. 01 Matrix.cpp
+++ 13. Graphs/2. Traversal Problems/05. 01 Matrix.cpp
@@ -16,6 +16,15 @@ COMPLEXITY ANALYSIS:
 - Space Complexity: O(m * n), where m is the number of rows and n is the number of columns in the matrix. We use additional space for the distance matrix and the queue during BFS.
 */
 
+/* Notes :
+BFS question
+Nearest 0's in LC (here) and nearest 1's in striver blog
+vis and dis 2D matrices are used
+We insert all the 0's into queue with nearest 0 distance as 0 and vis = 1
+We go in 4 directions and check if vis = 0 and mat[i][j] = 1
+At last we stop after going to all the nodes in matrix
+*/
+
 vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
     int m = mat.size(); 
     int n = mat[0].size();
@@ -50,6 +59,37 @@ vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
             }
         }
         level++;
+    }
+
+    return dis;
+}
+______________________________________________
+
+// Not optimal - unneccesary DFS calls are being made here, TLE comes
+
+void dfs(int x, int y, vector<vector<int>>& mat, vector<vector<int>>& dis, int dist) {
+    if (x < 0 || y < 0 || x >= mat.size() || y >= mat[0].size() || dis[x][y] <= dist)
+        return;
+
+    dis[x][y] = dist;
+
+    dfs(x - 1, y, mat, dis, dist + 1); // up
+    dfs(x + 1, y, mat, dis, dist + 1); // down
+    dfs(x, y - 1, mat, dis, dist + 1); // left
+    dfs(x, y + 1, mat, dis, dist + 1); // right
+}
+
+vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+    int m = mat.size();
+    int n = mat[0].size();
+    vector<vector<int>> dis(m, vector<int>(n, INT_MAX));
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (mat[i][j] == 0) {
+                dfs(i, j, mat, dis, 0);
+            }
+        }
     }
 
     return dis;
