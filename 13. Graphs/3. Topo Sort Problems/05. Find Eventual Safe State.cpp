8dee128cbb447a6be82a24748f6673ec7b23c8c3
Harsha731 <harshapraneeth51@gmail.com>
2025-01-03 17:19:06 +0530

    Update 05. Find Eventual Safe State.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 13. Graphs/3. Topo Sort Problems/05. Find Eventual Safe State.cpp
+++ 13. Graphs/3. Topo Sort Problems/05. Find Eventual Safe State.cpp
@@ -8,61 +8,63 @@ A node is a terminal node if there are no outgoing edges. A node is a safe node 
 from that node leads to a terminal node (or another safe node).
 
 APPROACH:
-- We are given a directed graph, and we need to find all the safe nodes.
-- A node is safe if every possible path starting from that node leads to a terminal node (or another safe node).
-- To solve this, we can use a reverse graph approach along with a topological sorting method.
-- We create a reverse adjacency list, where revadj[i] contains all the nodes from which there is a directed edge 
-to node i (i.e., nodes that have an edge towards node i).
-- We also keep track of the outdegree of each node, which represents the number of outgoing edges from that node.
-- We then start with nodes that have an outdegree of 0 (i.e., terminal nodes) and perform a BFS-like traversal.
-- During the traversal, we keep reducing the outdegree of the nodes from which there is a directed edge to the current node.
-- If a node's outdegree becomes 0 during the traversal, we add it to the queue for further processing.
-- All the nodes that become terminal nodes during the traversal are safe nodes.
+1. Initialize:
+    Set all nodes' indegree to 0.
+    Create a queue and push nodes with indegree 0.
+    Initialize an empty safeNodes array.
+
+2. Reverse Edges: Reverse the direction of all edges in the graph.
+3. Calculate Indegree: For each edge u -> v, increment indegree[v].
+
+4. BFS Traversal:
+    While the queue is not empty:
+        Pop a node, add to safeNodes.
+        For each adjacent node v, decrement indegree[v].
+        If indegree[v] == 0, push v to the queue.
+5. Sort: Sort the safeNodes array.
 
 COMPLEXITY ANALYSIS:
-- Time Complexity: O(N + E), where N is the number of nodes, and E is the number of edges in the graph. 
-We perform a BFS-like traversal of all nodes and edges.
-- Space Complexity: O(N + E), where N is the number of nodes, and E is the number of edges in the graph. 
-We use additional space to store the reverse adjacency list and outdegree of each node.
+Time Complexity: O(V+E)+O(N*logN), where V = no. of nodes and E = no. of edges. This is a simple BFS algorithm. 
+Extra O(N*logN) for sorting the safeNodes array, where N is the number of safe nodes.
+
+Space Complexity: O(N) + O(N) + O(N) ~ O(3N), O(N) for the indegree array, O(N) for the queue data structure 
+used in BFS(where N = no.of nodes), and extra O(N) for the adjacency list to store the graph in a reversed order.
 */
 
-vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-    int n = graph.size();
-    // Creating reverse adjacency list and outdegree array
-    vector<int> revadj[n];
-    vector<int> outdeg(n);
-    for (int i = 0; i < n; i++) {
-        outdeg[i] += graph[i].size();
-        for (auto j : graph[i]) {
-            revadj[j].push_back(i);
-        }
-    }
+// We have to remove the nodes in cycle as well as well as nodes leading to cycle
+// The nodes have to be leading to always a terminal node, else don't consider them 
+class Solution {
+public:
+	vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+		vector<int> adjRev[V];
+		int indegree[V] = {0};
+		for (int i = 0; i < V; i++) {
+			// i -> it
+			// it -> i
+			for (auto it : adj[i]) {
+				adjRev[it].push_back(i);
+				indegree[i]++;
+			}
+		}
+		queue<int> q;
+		vector<int> safeNodes;
+		for (int i = 0; i < V; i++) {
+			if (indegree[i] == 0) {
+				q.push(i);
+			}
+		}
 
-    queue<int> q;
-    for (int i = 0; i < n; i++) {
-        if (outdeg[i] == 0) q.push(i);
-    }
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			safeNodes.push_back(node);
+			for (auto it : adjRev[node]) {
+				indegree[it]--;
+				if (indegree[it] == 0) q.push(it);
+			}
+		}
 
-    vector<int> ans;
-
-    while (!q.empty()) {
-        int siz = q.size();
-        while (siz--) {
-            int node = q.front();
-            q.pop();
-            ans.push_back(node);
-
-            // Reduce outdegree of the nodes from which there is a directed edge to the current node
-            for (auto v : revadj[node]) {
-                outdeg[v]--;
-                if (outdeg[v] == 0) {
-                    q.push(v);
-                }
-            }
-        }
-    }
-
-    // Sort the safe nodes in ascending order
-    sort(ans.begin(), ans.end());
-    return ans;
-}
+		sort(safeNodes.begin(), safeNodes.end());
+		return safeNodes;
+	}
+};
