31ce85782d2e2e4f6872a9148d7480a8f4acdef8
Harsha731 <harshapraneeth51@gmail.com>
2025-01-03 20:07:18 +0530

    Update 05. Path with minimum effort.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 13. Graphs/4. Shortest Path Problems/05. Path with minimum effort.cpp
+++ 13. Graphs/4. Shortest Path Problems/05. Path with minimum effort.cpp
@@ -1,6 +1,9 @@
 /*QUESTION:
 
-You are a hiker preparing for an upcoming hike. You are given heights, a 2D array of size rows x columns, where heights[row][col] represents the height of cell (row, col). You are situated in the top-left cell, (0, 0), and you hope to travel to the bottom-right cell, (rows-1, columns-1) (i.e., 0-indexed). You can move up, down, left, or right, and you wish to find a route that requires the minimum effort.
+You are a hiker preparing for an upcoming hike. You are given heights, a 2D array of size rows x columns, 
+where heights[row][col] represents the height of cell (row, col). You are situated in the top-left cell, 
+(0, 0), and you hope to travel to the bottom-right cell, (rows-1, columns-1) (i.e., 0-indexed). 
+You can move up, down, left, or right, and you wish to find a route that requires the minimum effort.
 
 A route's effort is the maximum absolute difference in heights between two consecutive cells of the route.
 
@@ -27,7 +30,7 @@ COMPLEXITY ANALYSIS:
 CODE:
 */
 
-typedef pair<int, pair<int, int>> tri;
+typedef pair<int, pair<int, int>> tri;      // (effort, (x, y))
 
 int minimumEffortPath(vector<vector<int>>& heights) {
     int n = heights.size(), m = heights[0].size();
