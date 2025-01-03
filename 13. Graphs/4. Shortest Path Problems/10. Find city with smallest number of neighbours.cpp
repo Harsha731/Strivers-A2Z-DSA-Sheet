825a1a02bf6f2152b10c53958f36e4674a43a46f
Harsha731 <harshapraneeth51@gmail.com>
2025-01-03 22:26:55 +0530

    Update 10. Find city with smallest number of neighbours.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 13. Graphs/4. Shortest Path Problems/10. Find city with smallest number of neighbours.cpp
+++ 13. Graphs/4. Shortest Path Problems/10. Find city with smallest number of neighbours.cpp
@@ -18,64 +18,49 @@ City 2 -> [City 0, City 1, City 3]
 City 3 -> [City 1, City 2]
 Cities 0 and 3 have 2 neighboring cities at a distanceThreshold = 4, but we have to return city 3 since it has the greatest number.
 
-APPROACH:
-- We will use Dijkstra's algorithm to find the shortest distances from each city to all other cities in the graph.
-- For each city, we will find the count of cities that are reachable through some path and whose distance is at most distanceThreshold.
-- We will keep track of this count for each city in a map.
-- Finally, we will find the city with the smallest count, and if there are multiple such cities, we will return the city with the greatest number.
-
-COMPLEXITY ANALYSIS:
-- The time complexity of Dijkstra's algorithm is O(V^2), where V is the number of cities.
-- Since we perform Dijkstra's algorithm for each city, the overall time complexity is O(V^3).
-- The space complexity is O(V) for storing the distances and the count of reachable cities for each city.
+// Use Floyd warsall
+// TC : O(V^3) and SC : O(V^2)
 
 CODE:
 */
 
-int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-    vector<pair<int, int>> adj[n];
-    for (auto e : edges) {
-        adj[e[0]].push_back({e[1], e[2]});
-        adj[e[1]].push_back({e[0], e[2]});
-    }
+class Solution {
+public:
+	int findCity(int n, int m, vector<vector<int>>& edges, int distanceThreshold) {
+		vector<vector<int>> dist(n, vector<int> (n, INT_MAX));
+		for (auto it : edges) {
+			dist[it[0]][it[1]] = it[2];
+			dist[it[1]][it[0]] = it[2];
+		}
+		for (int i = 0; i < n; i++) dist[i][i] = 0;
 
-    vector<int> dis(n, numeric_limits<int>::max());
-    unordered_map<int, int> mp;
+		// Here, already the distances are INT_MAX, so no need to change from -1 to INT_MAX
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX)
+						continue;
+					dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+				}
+			}
+		}
 
-    for (int i = 0; i < n; i++) {
-        dis[i] = 0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, i});
+		int cntCity = n;
+		int cityNo = -1;
+		for (int city = 0; city < n; city++) {
+			int cnt = 0;
+			for (int adjCity = 0; adjCity < n; adjCity++) {
+				if (dist[city][adjCity] <= distanceThreshold)
+					cnt++;
+			}
 
-        while (!pq.empty()) {
-            auto [uwt, u] = pq.top();
-            pq.pop();
+			// We need to return the city with max under thresold, if ties, then return with more index number
+			if (cnt <= cntCity) {
+				cntCity = cnt;		// store the count
+				cityNo = city;		// store which city it is
+			}
+		}
+		return cityNo;
 
-            for (auto [v, vwt] : adj[u]) {
-                if (dis[v] > uwt + vwt) {
-                    dis[v] = uwt + vwt;
-                    pq.push({dis[v], v});
-                }
-            }
-        }
-
-        int nbrCnt = 0;
-        for (int j = 0; j < n; j++) {
-            if (i != j && dis[j] <= distanceThreshold)
-                nbrCnt++;
-            dis[j] = numeric_limits<int>::max();
-        }
-        mp[i] = nbrCnt;
-    }
-
-    int mini = numeric_limits<int>::max(), ans = 0;
-    for (int i = 0; i < n; i++) {
-        if (mp[i] < mini) {
-            mini = mp[i];
-            ans = i;
-        } else if (mp[i] == mini)
-            ans = max(ans, i);
-    }
-
-    return ans;
-}
+	}
+};
