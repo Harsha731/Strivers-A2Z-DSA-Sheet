154ab1e019836dc68ca857134dcdf23d7e573f9b
Harsha731 <harshapraneeth51@gmail.com>
2025-01-03 13:43:28 +0530

    Update 04. Detect Cycle in Undirected Graph.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 13. Graphs/2. Traversal Problems/04. Detect Cycle in Undirected Graph.cpp
+++ 13. Graphs/2. Traversal Problems/04. Detect Cycle in Undirected Graph.cpp
@@ -12,13 +12,56 @@ COMPLEXITY ANALYSIS:
 - Space Complexity: O(V), where V is the number of vertices. We use an additional array to keep track of visited nodes.
 */
 
-/* Notes :
-DFS, BFS both
-replace the 4 directionally connected pixels colors which are of 'same color' as the current cell to given target color
-i.e, image[nrow][ncol] == cur_color && image[nrow][ncol] != target_color
 
-If the start color = target color, simply return at the starting itself
-As it is unnecessary calls    
+/* BFS approach
+We push {node, parent} and we check parent!=adjNode if already visited as everytime parent is also a adjacent node which is already visited
+At start parent is -1
+We have three cases, visNode[] = 0
+visNode[] = 1 and it is parentNode
+visNode[] = 1 and it is not parentNode
+*/
+
+class Solution {
+  private: 
+  bool detect(int src, vector<int> adj[], int vis[]) {
+      vis[src] = 1; 
+      queue<pair<int,int>> q; 
+      q.push({src, -1}); 
+      while(!q.empty()) {
+          int node = q.front().first; 
+          int parent = q.front().second; 
+          q.pop(); 
+          for(auto adjacentNode: adj[node]) {
+              if(!vis[adjacentNode]) {
+                  vis[adjacentNode] = 1; 
+                  q.push({adjacentNode, node}); 
+              }
+              else if(parent != adjacentNode) {
+                  return true; 
+              }
+          }
+      }
+      return false; 
+  }
+  public:
+    bool isCycle(int V, vector<int> adj[]) {
+        int vis[V] = {0};
+        for(int i = 0;i<V;i++) {
+            if(!vis[i]) {
+                if(detect(i, adj, vis)) return true; 
+            }
+        }
+        return false; 
+    }
+};
+__________________________________________________________
+
+/* DFS approach
+Notes :
+We carry the parent node in the DFS calls for checking 
+We have three cases, visNode[] = 0
+visNode[] = 1 and it is parentNode
+visNode[] = 1 and it is not parentNode
 */
 
 bool dfs(int node, int parent, vector<int> adj[], vector<bool>& vis){
