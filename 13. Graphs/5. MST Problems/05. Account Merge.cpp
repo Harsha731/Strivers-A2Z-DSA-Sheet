5bbc1e913212955afe2d76fd991cf01c75e1ab61
Harsha731 <harshapraneeth51@gmail.com>
2025-01-04 15:01:38 +0530

    Update 05. Account Merge.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 13. Graphs/5. MST Problems/05. Account Merge.cpp
+++ 13. Graphs/5. MST Problems/05. Account Merge.cpp
@@ -1,87 +1,81 @@
 /*
 QUESTION:-
-Given a list of accounts where each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
-Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some common email to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
-After merging the accounts, return the accounts in the following format: the first element of each account is the name, and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
+Given a list of accounts where each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, 
+and the rest of the elements are emails representing emails of the account.
+Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some common email 
+to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could 
+have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
+After merging the accounts, return the accounts in the following format: the first element of each account is the name, 
+and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
+*/
 
-Approach:
-1. We use a disjoint-set data structure to group accounts that belong to the same person based on their common emails.
-2. Create a disjoint-set and an unordered map to store email to account index mapping.
-3. Iterate through the list of accounts, for each account, iterate through its emails, and add them to the unordered map with the corresponding account index as the value.
-4. Iterate through the unordered map and union accounts that share common emails using the disjoint-set.
-5. Create a set for each group of accounts and add the emails of each account to the corresponding set.
-6. Iterate through the disjoint-set and create the final merged accounts list by grouping emails for each account based on their root in the disjoint-set.
+/*
+what happens in the sort ?
+
+1) We keep on inserting the mails in umap(string, i), if there is any duplicate, we connect the i's
+
+2) Now, arrange in the actual order in adjacent list, here some i's may become empty which are not empty earlier
+
+3) Now, insert the user name of the account in the temp vector and push all the emails in adj list to temp vector
+Then, push the temp vector to ans 2D vector
+Here, we have to sort as the emails won't be in sorted order
+
+We are using 'ump' for mapping the email to its index
+Then, we are using '2D vector' for merging and inserting all the emails under same user. Here, some entries are empty too
+Then, '2D ans' vector is used for removing those empty 1D vectors and also we add username at start and also we sort them before returning
+
+Pass by reference to save TC and SC too much
 
 Complexity Analysis:
 - Let n be the number of accounts and m be the average number of emails per account.
 - The time complexity of this approach is O(n*m*α(n)), where α(n) is the inverse Ackermann function, which grows very slowly and is nearly constant.
 - The space complexity is O(n*m) to store the emails and their corresponding account indices in the unordered map, and O(n*m) to store the merged accounts in the final result.
+
 */
+__________________________
 
-class DisjointSet {
+class Solution {
 public:
-    vector<int> parent, size;
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accs) {
+        int n = accs.size();
+        DisjointSet ds(n);
+        unordered_map<string, int> emailMap;
 
-    DisjointSet(int n) {
-        size.resize(n, 1);
-        parent.resize(n);
-        for (int i = 0; i < n; i++)
-            parent[i] = i;
-    }
-
-    int findUPar(int node) {
-        if (parent[node] == node)
-            return node;
-        return parent[node] = findUPar(parent[node]);
-    }
-
-    void unionSize(int u, int v) {
-        int uP = findUPar(u), vP = findUPar(v);
-        int uS = size[uP], vS = size[vP];
-        if (uS > vS) {
-            parent[vP] = uP;
-            size[uP] += vS;
-        } else {
-            parent[uP] = vP;
-            size[vP] += uS;
+        // Build the disjoint set with email mapping
+        for (int i = 0; i < n; ++i) {
+            for (int j = 1; j < accs[i].size(); ++j) {
+                const string& email = accs[i][j];
+                if (emailMap.find(email) == emailMap.end()) {
+                    emailMap[email] = i;  // Map email to account index
+                } else {
+                    ds.unionBySize(i, emailMap[email]);
+                }
+            }
         }
+
+        // Group emails by their representative account
+        vector<vector<string>> emailGroups(n);
+        for (const auto& [email, idx] : emailMap) {
+            int root = ds.findUPar(idx);
+            emailGroups[root].push_back(email);
+        }
+
+        // Construct the result
+        vector<vector<string>> res;
+        for (int i = 0; i < n; ++i) {
+            if (emailGroups[i].empty()) continue;
+
+            // Sort the emails
+            sort(emailGroups[i].begin(), emailGroups[i].end());
+
+            // Prepare the merged account
+            vector<string> temp;
+            temp.push_back(accs[i][0]);  // Add the name
+            temp.insert(temp.end(), emailGroups[i].begin(), emailGroups[i].end());
+
+            res.push_back(temp);
+        }
+
+        return res;
     }
 };
-
-vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-    unordered_map<string, vector<int>> emailToAccountIndex;
-    int n = accounts.size();
-    DisjointSet djs(n);
-
-    for (int u = 0; u < n; u++) {
-        for (int i = 1; i < accounts[u].size(); i++) {
-            emailToAccountIndex[accounts[u][i]].push_back(u);
-        }
-    }
-
-    for (auto [email, users] : emailToAccountIndex) {
-        for (int i = 1; i < users.size(); i++) {
-            if (djs.findUPar(users[i - 1]) != djs.findUPar(users[i]))
-                djs.unionSize(users[i - 1], users[i]);
-        }
-    }
-
-    set<string> userMail[n];
-    for (int i = 0; i < n; i++) {
-        for (int m = 1; m < accounts[i].size(); m++) {
-            userMail[djs.findUPar(i)].insert(accounts[i][m]);
-        }
-    }
-
-    vector<vector<string>> ans;
-    for (int i = 0; i < n; i++) {
-        if (userMail[i].empty()) continue;
-        vector<string> temp;
-        temp.push_back(accounts[i][0]);
-        for (auto m : userMail[i])
-            temp.push_back(m);
-        ans.push_back(temp);
-    }
-
-    return ans;
-}
