af79f0695f6750f05e218afa1ab3ea3b82d68e34
Harsha731 <harshapraneeth51@gmail.com>
2025-01-05 00:34:31 +0530

    Update 03. Number of Operations to make Network.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 13. Graphs/5. MST Problems/03. Number of Operations to make Network.cpp
+++ 13. Graphs/5. MST Problems/03. Number of Operations to make Network.cpp
@@ -79,3 +79,42 @@ int makeConnected(int n, vector<vector<int>>& connections) {
     int ans = djs.disconnected(n);
     return ans - 1;
 }
+_______________________________________________________
+
+/*
+DFS / BFS approach
+We are checking at start, m < n-1
+Then, we are counting the no. of BFS traversals needed. so, cnt - 1 is answer
+*/  
+int makeGraphConnected(int n, vector<pair<int, int>>& edges, int m) {
+    if (m < n - 1) return -1;
+
+    vector<int> adj[n + 1];
+    for (auto& edge : edges) {
+        adj[edge.first].push_back(edge.second);
+        adj[edge.second].push_back(edge.first);
+    }
+
+    vector<bool> vis(n + 1, false);
+    int ctr = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i]) {
+            queue<int> q;
+            vis[i] = true;
+            q.push(i);
+            ctr++;
+            while (!q.empty()) {
+                int u = q.front(); q.pop();
+                for (int v : adj[u]) {
+                    if (!vis[v]) {
+                        vis[v] = true;
+                        q.push(v);
+                    }
+                }
+            }
+        }
+    }
+
+    return ctr - 1;
+}
