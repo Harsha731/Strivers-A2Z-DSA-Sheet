7655d15005046fb8b38ef0d3f33ff1c2d82d1b97
Harsha731 <harshapraneeth51@gmail.com>
2025-01-05 12:07:29 +0530

    Update 03. Unique Paths 2.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 14. Dynamic Programming/3. 2D DP/03. Unique Paths 2.cpp
+++ 14. Dynamic Programming/3. 2D DP/03. Unique Paths 2.cpp
@@ -1,6 +1,8 @@
 /*
 QUESTION:-
-You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). 
+The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or 
+right at any point in time.
 
 An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
 
@@ -16,16 +18,22 @@ There are two ways to reach the bottom-right corner:
 
 Approach:
 1. We can solve this problem using dynamic programming with memoization (top-down approach).
-2. We define a helper function fmemo(i, j, grid, dp) that calculates the number of unique paths from the top-left corner (0, 0) to the cell (i, j) on the grid.
-3. The function checks if the number of unique paths for the cell (i, j) is already calculated and stored in the dp array. If yes, it returns the value from dp.
-4. If the cell (i, j) contains an obstacle (i.e., grid[i][j] == 1), then there is no possible path through that cell, so the function returns 0.
-5. Otherwise, it calculates the number of unique paths for the cell (i, j) by adding the number of unique paths from the cell above (i-1, j) and the cell to the left (i, j-1).
+2. We define a helper function fmemo(i, j, grid, dp) that calculates the number of unique paths from the 
+    top-left corner (0, 0) to the cell (i, j) on the grid.
+3. The function checks if the number of unique paths for the cell (i, j) is already calculated and 
+    stored in the dp array. If yes, it returns the value from dp.
+4. If the cell (i, j) contains an obstacle (i.e., grid[i][j] == 1), then there is no possible path 
+    through that cell, so the function returns 0.
+5. Otherwise, it calculates the number of unique paths for the cell (i, j) by adding the number of 
+    unique paths from the cell above (i-1, j) and the cell to the left (i, j-1).
 6. The base case is when i or j is less than 0, in which case the function returns 0 as there are no paths to reach those cells.
-7. In the main function uniquePathsWithObstacles(obstacleGrid), we create a dp array of size m x n initialized with -1 and call the fmemo function for the bottom-right cell (m-1, n-1).
+7. In the main function uniquePathsWithObstacles(obstacleGrid), we create a dp array of size m x n 
+    initialized with -1 and call the fmemo function for the bottom-right cell (m-1, n-1).
 8. Finally, we return the number of unique paths obtained for the bottom-right cell as the result.
 
 Complexity Analysis:
-- The time complexity of the fmemo function is O(m x n) because we are calculating the number of unique paths for each cell only once and storing it in the dp array.
+- The time complexity of the fmemo function is O(m x n) because we are calculating the number of 
+    unique paths for each cell only once and storing it in the dp array.
 - The space complexity is also O(m x n) to store the dp array.
 
 Note: The given test cases are generated such that the answer will be less than or equal to 2 x 10^9.
