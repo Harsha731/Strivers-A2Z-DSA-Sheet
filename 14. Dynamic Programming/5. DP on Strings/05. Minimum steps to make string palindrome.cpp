b53e6b359365bb1c805ad9526e646ec44b85c4fe
Harsha731 <harshapraneeth51@gmail.com>
2025-01-05 16:37:20 +0530

    Update 05. Minimum steps to make string palindrome.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 14. Dynamic Programming/5. DP on Strings/05. Minimum steps to make string palindrome.cpp
+++ 14. Dynamic Programming/5. DP on Strings/05. Minimum steps to make string palindrome.cpp
@@ -18,7 +18,7 @@ COMPLEXITY ANALYSIS:
 CODE:
 */
 
-
+// Memoization
 int fmemo(int i, int j, string &s, string &r, vector<vector<int>> &dp) {
     if (i < 0 || j < 0)
         return 0;
@@ -30,6 +30,32 @@ int fmemo(int i, int j, string &s, string &r, vector<vector<int>> &dp) {
         return dp[i][j] = fmemo(i - 1, j - 1, s, r, dp) + 1;
     else
         return dp[i][j] = max(fmemo(i - 1, j, s, r, dp), fmemo(i, j - 1, s, r, dp));
+}
+
+// Tabulation
+int ftab(int n, int m, string& s, string& r){
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(s[i-1]==r[j-1]) dp[i][j] = dp[i-1][j-1]+1;
+            else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    return dp[n][m];
+}
+
+// Space Optimization
+int fopt(int n, int m, string& s, string& r){
+    vector<int> prev(m+1,0);
+    for(int i=1; i<=n; i++){
+        vector<int> curr(m+1,0);
+        for(int j=1; j<=m; j++){
+            if(s[i-1]==r[j-1]) curr[j] = prev[j-1]+1;
+            else curr[j] = max(prev[j],curr[j-1]);
+        }
+        prev = curr;
+    }
+    return prev[m];
 }
 
 int minInsertions(string s) {
