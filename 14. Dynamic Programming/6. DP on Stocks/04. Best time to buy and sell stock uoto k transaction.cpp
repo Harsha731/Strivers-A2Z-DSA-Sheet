12a07da937d71f26bc8e3500d9500fd93be3bcb8
Harsha731 <harshapraneeth51@gmail.com>
2025-01-06 16:26:43 +0530

    Update 04. Best time to buy and sell stock uoto k transaction.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 14. Dynamic Programming/6. DP on Stocks/04. Best time to buy and sell stock uoto k transaction.cpp
+++ 14. Dynamic Programming/6. DP on Stocks/04. Best time to buy and sell stock uoto k transaction.cpp
@@ -27,6 +27,8 @@ COMPLEXITY ANALYSIS:
 CODE:
 */
 
+// Memoization
+
 int maxi;
 int fmemo(int i, int hold, int cap, vector<int>& prices, vector<vector<vector<int>>>& dp){
     if(i == prices.size()) return 0;
@@ -55,4 +57,66 @@ int maxProfit(int k, vector<int>& prices) {
     maxi = k;
     vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k + 1, -1)));
     return fmemo(0, 0, 0, prices, dp);
+}
+___________________________________________
+
+// Tabulation
+
+/*
+dp[][][cap=0]  =  0
+dp[n][][]      =  0
+For selling situation, we consider, dp[][][cap] = dp[][][cap-1]	
+There are cap+1 tables of n*2 size
+*/
+class Solution {
+public:
+    int maximumProfit(vector<int>& Arr, int n, int k) {
+        // Creating a 3D DP array of size [n+1][2][k+1] initialized to 0
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(k + 1, 0)));
+
+        // Loop through the stock prices from the end to the beginning
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= k; cap++) {
+                    if (buy == 0) { // We can buy the stock
+                        dp[ind][buy][cap] = max(0 + dp[ind + 1][0][cap], -Arr[ind] + dp[ind + 1][1][cap]);
+                    }
+
+                    if (buy == 1) { // We can sell the stock
+                        dp[ind][buy][cap] = max(0 + dp[ind + 1][1][cap], Arr[ind] + dp[ind + 1][0][cap - 1]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][0][k];
+    }
+
+};
+_____________________________________________
+
+// Space optimization
+
+int maxProfit(vector<int>& Arr, int n, int k) {
+    // Create two arrays, 'ahead' and 'cur', to track the maximum profit at each step
+    vector<vector<int>> ahead(2, vector<int>(k + 1, 0));
+    vector<vector<int>> cur(2, vector<int>(k + 1, 0));
+
+    for (int ind = n - 1; ind >= 0; ind--) {
+        for (int buy = 0; buy <= 1; buy++) {
+            for (int cap = 1; cap <= k; cap++) {
+                if (buy == 0) { // We can buy the stock
+                    cur[buy][cap] = max(0 + ahead[0][cap], -Arr[ind] + ahead[1][cap]);
+                }
+
+                if (buy == 1) { // We can sell the stock
+                    cur[buy][cap] = max(0 + ahead[1][cap], Arr[ind] + ahead[0][cap - 1]);
+                }
+            }
+        }
+        // Update the 'ahead' array with the current values
+        ahead = cur;
+    }
+
+    return ahead[0][k];
 }
