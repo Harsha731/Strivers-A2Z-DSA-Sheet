543f1e2c71b8a90f8f00ab5ce6ee5d4177bfd8ae
Harsha731 <harshapraneeth51@gmail.com>
2025-01-06 18:54:06 +0530

    Update 01. Longest Increasing Subsequence.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 14. Dynamic Programming/7. DP on LIS/01. Longest Increasing Subsequence.cpp
+++ 14. Dynamic Programming/7. DP on LIS/01. Longest Increasing Subsequence.cpp
@@ -5,7 +5,7 @@ Given an integer array nums, return the length of the longest strictly increasin
 APPROACH:
 - This problem can be solved using dynamic programming.
 - Let dp[i] represent the length of the longest increasing subsequence ending at index i.
-- For each element at index i, we iterate through all the previous indices (prev) from 0 to i-1.
+- For each element at index i, we iterate through all the previous indices (prev) from 0 to i-1
 - If nums[prev] is smaller than nums[i], it means we can extend the increasing subsequence ending at prev by including nums[i].
 - We update dp[i] as max(dp[prev] + 1, dp[i]).
 - Initialize a variable ans to keep track of the maximum value in dp.
@@ -19,21 +19,77 @@ COMPLEXITY ANALYSIS:
 CODE:
 */
 
-int ftab(int n, vector<int>& nums) {
-    vector<int> dp(n, 1);
-    for (int i = 0; i < n; i++) {
-        for (int prev = 0; prev < i; prev++) {
-            if (nums[prev] < nums[i]) {
-                dp[i] = max(dp[prev] + 1, dp[i]);
-            }
-        }
+// We can write in reverse also for each method
+
+// Memoization
+int getAns(int arr[], int n, int ind, int prev_index, vector<vector<int>>& dp) {
+    // Base condition
+    if (ind == n)
+        return 0;
+        
+    if (dp[ind][prev_index + 1] != -1)
+        return dp[ind][prev_index + 1];
+    
+    int notTake = 0 + getAns(arr, n, ind + 1, prev_index, dp);
+    int take = 0;
+    
+    if (prev_index == -1 || arr[ind] > arr[prev_index]) {
+        take = 1 + getAns(arr, n, ind + 1, ind, dp);
     }
-    int ans = -1;
-    for (auto i : dp) ans = max(ans, i);
-    return ans;
+    
+    return dp[ind][prev_index + 1] = max(notTake, take);
 }
 
-int lengthOfLIS(vector<int>& nums) {
-    int n = nums.size();
-    return ftab(n, nums);
+int longestIncreasingSubsequence(int arr[], int n) {
+    vector<vector<int>> dp(n, vector<int>(n + 1, -1));    
+    return getAns(arr, n, 0, -1, dp);
 }
+_____________________________________
+
+// Tabulation
+
+int longestIncreasingSubsequence(int arr[], int n) {
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+    // Process the table from bottom to top
+    for (int ind = n - 1; ind >= 0; ind--) {
+        for (int prev_index = ind - 1; prev_index >= -1; prev_index--) {
+            int notTake = dp[ind + 1][prev_index + 1];
+            
+            int take = 0;
+            if (prev_index == -1 || arr[ind] > arr[prev_index]) {
+                take = 1 + dp[ind + 1][ind + 1];
+            }
+            dp[ind][prev_index + 1] = max(notTake, take);
+        }
+    }
+    return dp[0][0];
+}
+___________________________
+
+// Space Optimization
+
+class Solution {
+public:
+
+int lengthOfLIS(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> next(n + 1, 0), curr(n + 1, 0);
+
+    // Process from bottom to top
+    for (int ind = n - 1; ind >= 0; ind--) {
+        for (int prev_index = ind - 1; prev_index >= -1; prev_index--) {
+            int notTake = next[prev_index + 1];
+            
+            int take = 0;
+            if (prev_index == -1 || arr[ind] > arr[prev_index]) {
+                take = 1 + next[ind + 1];
+            }
+            curr[prev_index + 1] = max(notTake, take);
+        }
+        next = curr;
+    }
+    return next[0];
+}
+
+};
