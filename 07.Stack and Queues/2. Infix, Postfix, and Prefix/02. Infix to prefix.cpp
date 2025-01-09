4c01c8f5acf135de6897ec7b4402d92fa9ab554f
Harsha731 <harshapraneeth51@gmail.com>
2025-01-09 12:41:21 +0530

    Update 02. Infix to prefix.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 07.Stack and Queues/2. Infix, Postfix, and Prefix/02. Infix to prefix.cpp
+++ 07.Stack and Queues/2. Infix, Postfix, and Prefix/02. Infix to prefix.cpp
@@ -19,42 +19,88 @@ After converting the infix expression into prefix expression, the resultant expr
 CODE:
 */
 
-string INFIX_PREFIX(string s) {
-    reverse(s.begin(), s.end());
-    for (char &ch : s) {
-        if (ch == '(') ch = ')';
-        else if (ch == ')') ch = '(';
-    }
+/*  IMPORTANT  
+-> When Changing to <:
+The change from <= to < would prevent operators with equal precedence from being popped.
+Left-associative operators should not prevent other operators with the same precedence from being popped, 
+because they need to process from left to right.
+So, if you use <, only operators with strictly higher precedence will be popped, ensuring that we don't prematurely pop the same precedence operators.
 
-    stack<char> st;
-    string ans;
+-> For instance, when processing the expression a + b + c:
+After encountering the first +, it will be pushed onto the stack.
+When the second + is encountered, we need to pop the previous + (since + is left-associative) to ensure that the operators are placed in the correct order.
 
-    for (char ch : s) {
-        if (isalnum(ch)) {
-            ans += ch;
-        } else if (ch == '(') {
-            st.push(ch);
-        } else if (ch == ')') {
-            while (st.top() != '(') {
-                ans += st.top(); st.pop();
+-> In this case:
+The first + will be pushed onto the stack.
+The second + will trigger the loop, and the first + will be popped from the stack before the second + is pushed.
+*/
+
+bool isOperator(char c) {
+    return (!isalpha(c) && !isdigit(c));
+}
+
+int getPriority(char c) {
+    if (c == '^') return 3;
+    if (c == '*' || c == '/') return 2;
+    if (c == '+' || c == '-') return 1;
+    return 0;
+}
+
+string infixToPostfix(const string& infix) {
+    string output;
+    stack<char> charStack;
+    string expr = '(' + infix + ')';
+    
+    for (char c : expr) {
+        if (isalnum(c)) {
+            output += c;
+        } else if (c == '(') {
+            charStack.push(c);
+        } else if (c == ')') {
+            while (charStack.top() != '(') {
+                output += charStack.top();
+                charStack.pop();
             }
-            st.pop();
-        } else {
-            while (!st.empty() && precedence(ch) < precedence(st.top())) {
-                ans += st.top(); st.pop();
+            charStack.pop();
+        } else if (isOperator(c)) {
+            // Handle the '^' operator separately
+            if (c == '^') {
+                while (!charStack.empty() && getPriority(c) <= getPriority(charStack.top())) {
+                    output += charStack.top();
+                    charStack.pop();
+                }
+            } else {
+                // Handle the other operators ('+', '-', '*', '/')
+                while (!charStack.empty() && getPriority(c) < getPriority(charStack.top())) {
+                    output += charStack.top();
+                    charStack.pop();
+                }
             }
-            st.push(ch);
+            charStack.push(c);
         }
     }
 
-    while (!st.empty()) {
-        ans += st.top(); st.pop();
+    while (!charStack.empty()) {
+        output += charStack.top();
+        charStack.pop();
     }
 
-    reverse(ans.begin(), ans.end());
-    return ans;
+    return output;
 }
 
+string infixToPrefix(string infix) {
+    reverse(infix.begin(), infix.end());
+    
+    for (char& c : infix) {
+        if (c == '(') c = ')';
+        else if (c == ')') c = '(';
+    }
+
+    string prefix = infixToPostfix(infix);
+    reverse(prefix.begin(), prefix.end());
+
+    return prefix;
+}
 
 /*
 COMPLEXITY ANALYSIS:
