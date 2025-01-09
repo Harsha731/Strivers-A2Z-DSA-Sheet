fb8666a4e2d8210a3d71c950e7e4fac4f065bc25
Harsha731 <harshapraneeth51@gmail.com>
2025-01-09 10:41:41 +0530

    Update 03. Implement stack using queue.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 07.Stack and Queues/1. Learning/03. Implement stack using queue.cpp
+++ 07.Stack and Queues/1. Learning/03. Implement stack using queue.cpp
@@ -5,49 +5,56 @@ Implement a Stack using one queue.
 Approach:
 - We use the class `QueueStack` to represent the stack.
 - The stack is implemented using one queue `q`.
-- The `push(int)` function inserts an element into `q`.
-- The `pop()` function transfers `n-1` elements from front of `q` to it's back and returns the last element from `q` as the popped element.
-
+- One queue is needed
+- push - we insert the element we needed to insert at the end and we reinsert all the elements in front of this current element at the back
 
 Code:
 */
 
-class QueueStack {
-private:
-    std::queue<int> q;
-
+class MyStack {
 public:
-    void push(int x);
-    int pop();
-};
-
-void QueueStack::push(int x)
-{
-    q.push(x);
-}
-
-int QueueStack::pop()
-{
-    if (q.empty())
-        return -1;
-
-    for(int i=0; i<q.size()-1; i++){
-        int temp = q.front();
-        q.pop();
-        q.push(temp);
+	queue<int> que;
+    MyStack() { }
+        
+    void push(int x) {
+        que.push(x);
+        for(int i=0;i<que.size()-1;++i){
+            que.push(que.front());
+            que.pop();
+        }
     }
 
-    int popped = q.front();
-    q.pop();
+    // Removes the element on top of the stack.
+    int pop() {
+        int topVal = que.front();
+        que.pop();
+        return topVal;
+    }
 
-    return popped;
-}
+    // Get the top element.
+    int top() {
+        return que.front();
+    }
 
+    // Return whether the stack is empty.
+    bool empty() {
+        return que.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
 
 /*
 Complexity Analysis:
-- The `push()` operation has a time complexity of O(1) since we only need to enqueue an element into `q`.
-- The `pop()` operation has a time complexity of O(N) in the worst case, where N is the number of elements in `q`.
+- The `push()` operation has a time complexity of O(N)
+- The `pop()` operation has a time complexity of O(1)
 - The space complexity is O(N), where N is the total number of elements stored in the queue.
 */
 
