76876afd883b1dd76d41ec400290a6b171b0bead
Harsha731 <harshapraneeth51@gmail.com>
2025-01-11 12:31:02 +0530

    Add files via upload
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 08. Sliding Window/max points you can obtain from cards.cpp
+++ 08. Sliding Window/max points you can obtain from cards.cpp
@@ -1,5 +1,5 @@
-// We had did in reverse, we can also do in direct way as follows,
-// first calculate the k indexes sum, then subtract each and add on right side
+We had did in reverse, we can also do in direct way as follows,
+first calculate the k indexes sum, then subtract each and add on right side
 
 class Solution {
 public:
@@ -29,37 +29,5 @@ public:
             }
         }
         return totalSum - minSum;
-    }
-};
-
-
-// ______________________________
-
-// latest approach of finding the minimum window sum of n-k elements and the reverse is the max
-
-class Solution {
-public:
-    int maxScore(vector<int>& cards, int k) {
-        int total = 0;
-        int n = cards.size();
-
-        for (int card : cards) {
-            total += card;
-        }
-
-        // Calculate the initial sum of the first (n - k) elements
-        int minWindow = 0;
-        for (int i = 0; i < n - k; ++i) {
-            minWindow += cards[i];
-        }
-
-        // Use a sliding window to find the minimum sum of any subarray of size (n - k)
-        int currentWindow = minWindow;
-        for (int i = n - k; i < n; ++i) {
-            currentWindow += cards[i] - cards[i - (n - k)];
-            minWindow = min(minWindow, currentWindow);
-        }
-
-        return total - minWindow;
     }
 };
