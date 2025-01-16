1d047e5596d1f75ee3f0e3f5ca7652c394e75f90
Harsha731 <harshapraneeth51@gmail.com>
2025-01-16 22:41:51 +0530

    Update 03. KMP or Z string matching algo.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 16. Strings (Hard)/Hard/03. KMP or Z string matching algo.cpp
+++ 16. Strings (Hard)/Hard/03. KMP or Z string matching algo.cpp
@@ -15,35 +15,37 @@ Complexity Analysis:
 CODE:
 */
 
-// The KMP algorithm uses a preprocessing step to build a partial match table (also known as the "prefix" table), 
-// which helps in skipping unnecessary comparisons when a mismatch occurs.
+/*
+The KMP algorithm uses a preprocessing step to build a partial match table (also known as the "prefix" table), 
+which helps in skipping unnecessary comparisons when a mismatch occurs.
 
-// TC : O(M+N) and SC : O(1)
+TC : O(M+N) and SC : O(1)
 
-// a b c d e a b f a b c
-// 0 0 0 0 0 1 2 0 1 2 3
+Abdul Bari example : a b a b c a b c a b a b a b d
 
-// a a a a b a a c d
-// 0 1 2 3 0 1 2 0 0 
+a b a b d
+0 0 1 2 0
 
-// Abdul Bari example : a b a b c a b c a b a b a b d
+If string was not matching at d, we don't go to first a
+instead, we go second a
+It is because this suffix have a number assigned to it, so it is also a prefix, we can try from there
+Even if it fails, we go to the start
 
-// a b a b d
-// 0 0 1 2 0
+Abdul bari tells to compare i and j+1, but here we do i and j
 
-// If string was not matching at d, we don't go to first a
-// instead, we go second a
-// It is because this suffix have a number assigned to it, so it is also a prefix, we can try from there
-// Even if it fails, we go to the start
+a a b a a a c  is the pattern
+0 1 0 1 2 2 0
+Here, check the last 'a', it is not matching with the 'b', so we do len=lps[j-1], we check if this suffix is already present or not, if present we will go there
 
-// Abdul bari tells to compare i and j+1, but here we do i and j
 
-// a a b a a a c  is the pattern
-// 0 1 0 1 2 2 0
-// Here, check the last 'a', it is not matching with the 'b', so we do len=lps[j-1], we check if this suffix is already present or not, if present we will go there
-// 
+** The intution is that, what if this suffix in pattern becomes a prefix for a new one in LPS array calculation
 
-// ** The intution is that, what if this suffix in pattern becomes a prefix for a new one in LPS array calculation
+LPS = Longest Prefix which is also Suffix
+We do LPS for pattern (the smaller string) and not for the bigger one
+
+*/
+LPS = Longest Prefix which is also Suffix
+We do LPS for pattern (the smaller string) and not for the bigger one
 
 // Function to compute the prefix table (also called the longest prefix-suffix table)
 void computeLPS(const string& pattern, int lps[]) {
