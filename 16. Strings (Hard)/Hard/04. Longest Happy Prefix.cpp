e5b434cb9e42f3eb5c9d7aefe8e71026dd055f5a
Harsha731 <harshapraneeth51@gmail.com>
2025-01-19 12:32:37 +0530

    Update 04. Longest Happy Prefix.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 16. Strings (Hard)/Hard/04. Longest Happy Prefix.cpp
+++ 16. Strings (Hard)/Hard/04. Longest Happy Prefix.cpp
@@ -10,6 +10,14 @@ Output: "l"
 Explanation: s contains 4 prefixes excluding itself ("l", "le", "lev", "leve") and suffixes ("l", "el", "vel", "evel"). The largest prefix that is also a suffix is "l".
 
 Approach:
+
+We can add letters at the front only
+In the worst case, we need to add n-1 letters at the front
+KMP algo, do precomputation of LPS array
+
+aacecaaa$aaacecaa	
+The last a gives LPS[] = 7. So, the first 7 letters are in palindrome, The next are not. So, do s.substr(7) and keep that at front by reversing
+
 - We can solve this problem using the KMP (Knuth-Morris-Pratt) algorithm.
 - First, we calculate the LPS (Longest Prefix Suffix) array for the given string s.
 - The last value in the LPS array, lps[n-1], represents the length of the longest prefix that is also a suffix.
