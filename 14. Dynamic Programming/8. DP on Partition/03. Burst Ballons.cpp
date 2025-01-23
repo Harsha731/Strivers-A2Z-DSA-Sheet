d4ed3f7b665ed467e7a647808508aebda9ea179f
Harsha731 <harshapraneeth51@gmail.com>
2025-01-23 15:19:08 +0530

    Update 03. Burst Ballons.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 14. Dynamic Programming/8. DP on Partition/03. Burst Ballons.cpp
+++ 14. Dynamic Programming/8. DP on Partition/03. Burst Ballons.cpp
@@ -51,4 +51,39 @@ int maxCoins(vector<int>& nums) {
     vector<vector<int>> dp(n, vector<int>(n, -1));
     return fmemo(i, j, nums, dp);
 }
+___________________________________________________
+
+// Tabulation
+// No Space Optimization here
+// We assume the 'ind' ballon is burst at the end, so we do these 2 first - dp(i, ind-1) and dp(ind+1, j)
+// We calculate like this for every (i, j) pair
+
+int maxCoins(vector<int> &nums) {
+    int n = nums.size();
+    
+    nums.insert(nums.begin(), 1);
+    nums.push_back(1);
+    
+    vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= n; j++) {        // We can put j=i to n instead
+            if (i > j) continue;
+            int maxi = INT_MIN;
+            
+            // Iterate through each possible balloon to burst (last)
+            for (int ind = i; ind <= j; ind++) {
+                // Calculate the coins obtained by bursting the ind-th balloon (last)
+                int coins = nums[i - 1] * nums[ind] * nums[j + 1];
+                
+                int remainingCoins = dp[i][ind - 1] + dp[ind + 1][j];                
+                maxi = max(maxi, coins + remainingCoins);
+            }
+            
+            dp[i][j] = maxi;
+        }
+    }
+    
+    return dp[1][n];
+}
 
