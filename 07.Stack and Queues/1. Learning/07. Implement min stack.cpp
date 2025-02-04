4aacb160ab036b0fbe6cb5fff0d45a4308a182a0
Harsha731 <harshapraneeth51@gmail.com>
2025-02-04 15:06:40 +0530

    Update 07. Implement min stack.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 07.Stack and Queues/1. Learning/07. Implement min stack.cpp
+++ 07.Stack and Queues/1. Learning/07. Implement min stack.cpp
@@ -148,4 +148,27 @@ public:
         return mini;
     }
 };
+____________________________________________
 
+// This is best approach
+// Time: O(1) for all
+// Space: O(N)
+class MinStack {
+    stack<int> mn, val;
+public:
+    MinStack() {}
+    void push(int x) {
+        val.push(x);
+        if (mn.empty() || mn.top() >= x) mn.push(x);
+    }
+    void pop() {
+        if (val.top() == mn.top()) mn.pop();
+        val.pop();
+    }
+    int top() {
+        return val.top();
+    }
+    int getMin() {
+        return mn.top();
+    }
+};
