3b7dccd1623833081faaec89edf574e0a12d83a9
Harsha731 <harshapraneeth51@gmail.com>
2025-06-16 16:02:26 +0530

    Update 05.Number_of_subarray_sum_equal_k.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 01.Arrays/2.Medium/05.Number_of_subarray_sum_equal_k.cpp
+++ 01.Arrays/2.Medium/05.Number_of_subarray_sum_equal_k.cpp
@@ -20,28 +20,17 @@ To find the total number of subarrays with sum equal to k, we can use the techni
 CODE:
 */
 
-int subarraySum(vector<int> &nums, int k)
-{
-    int pref_sum = 0;
-    unordered_map<int, int> mp;
-    int ans = 0;
+int subarraySum(vector<int>& nums, int k) {
+    unordered_map<int, int> mp = {{0, 1}};
+    int sum = 0, count = 0;
 
-    for (int i = 0; i < nums.size(); i++)
-    {
-        pref_sum += nums[i];
-
-        if (pref_sum == k)
-            ans++;
-
-        if (mp.find(pref_sum - k) != mp.end())
-        {
-            ans += mp[pref_sum - k];
-        }
-
-        mp[pref_sum]++;
+    for (int num : nums) {
+        sum += num;
+        count += mp[sum - k];
+        mp[sum]++;
     }
 
-    return ans;
+    return count;
 }
 
 /*
