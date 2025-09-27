be2e1619835ca09a5a70e9aa3b43e8da84403db5
Harsha731 <harshapraneeth51@gmail.com>
2025-09-27 13:19:58 +0530

    Update 3.Search_in_rowwise_sorted_matrix.cpp
    Signed-off-by: Harsha731 <harshapraneeth51@gmail.com>

--- 02.Binary Search/2D Arrays/3.Search_in_rowwise_sorted_matrix.cpp
+++ 02.Binary Search/2D Arrays/3.Search_in_rowwise_sorted_matrix.cpp
@@ -39,6 +39,22 @@ We can start the search from the top-right element or the bottom-left element an
 CODE:
 */
 
+// TC: O(NlogM)
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size(), m = matrix[0].size();
+    for (int i = 0; i < n; i++) {
+        int l = 0, r = m - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (matrix[i][mid] == target) return true;
+            else if (matrix[i][mid] > target) r = mid - 1;
+            else l = mid + 1;
+        }
+    }
+    return false;
+}
+
+// staircase approach : O(N+M)
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
     int row = 0, col = matrix[0].size() - 1;
     while (row < matrix.size() && col >= 0) {
